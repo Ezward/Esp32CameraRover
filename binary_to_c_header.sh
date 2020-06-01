ec8b9eb5fb293c1de8b8edd6418b092fccb217dd
Ezward <ed@junelife.com>
2020-05-31 23:04:27 -0700

    initial commit.  Added rover commands.  Still need to add the ui and test it.

---
+++ binary_to_c_header.sh
@@ -0,0 +1,10 @@
+
+echo "#define index_ov2640_html_gz_len sizeof(index_ov2640_html_gz)" > $2
+echo "const uint8_t index_ov2640_html_gz[] = {" >> $2
+
+#
+# gzip file and convert file to c-language array of hex literals
+#
+gzip -c $1 | hexdump -v -e '16/1 "_x%02X" "\n"' | sed 's/_/\\/g; s/\\x  //g; s/.*/    "&"/' >> $2
+
+echo "};" >> $2
---
+++ rover_l9110s.cpp
@@ -0,0 +1,69 @@
+#include <analogWrite.h>  // shim for analogWrite() on ESP32
+
+/******************************************************/
+/****************rover control ************************/
+/******************************************************/
+byte speed = 255;
+
+int AIA_PIN = -1;
+int AIB_PIN = -1;
+int BIA_PIN = -1;
+int BIB_PIN = -1;
+
+//
+// set the rover pins
+//
+void roverInit(int a1, int a2, int b1, int b2) {
+  AIA_PIN = a1;
+  AIB_PIN = a2;
+  BIA_PIN = b1;
+  BIB_PIN = b2;
+}
+
+void roverSetSpeed(byte inSpeed) {
+  speed = inSpeed;
+}
+byte roverGetSpeed() {
+  return speed;
+}
+
+void roverStop() {
+  if (-1 == AIA_PIN) return;
+  
+  analogWrite(AIA_PIN, 0);
+  analogWrite(AIB_PIN, 0);
+  analogWrite(BIA_PIN, 0);
+  analogWrite(BIB_PIN, 0);
+}
+void roverForward(){
+  if (-1 == AIA_PIN) return;
+
+  analogWrite(AIA_PIN, speed);
+  analogWrite(AIB_PIN, 0);
+  analogWrite(BIA_PIN, speed);
+  analogWrite(BIB_PIN, 0);
+}
+void roverReverse(){
+  if (-1 == AIA_PIN) return;
+
+  analogWrite(AIA_PIN, 0);
+  analogWrite(AIB_PIN, speed);
+  analogWrite(BIA_PIN, 0);
+  analogWrite(BIB_PIN, speed);
+}
+void roverTurnRight() {
+  if (-1 == AIA_PIN) return;
+
+  analogWrite(AIA_PIN, speed);
+  analogWrite(AIB_PIN, 0);
+  analogWrite(BIA_PIN, 0);
+  analogWrite(BIB_PIN, speed);
+}
+void roverTurnLeft() {
+  if (-1 == AIA_PIN) return;
+
+  analogWrite(AIA_PIN, 0);
+  analogWrite(AIB_PIN, speed);
+  analogWrite(BIA_PIN, speed);
+  analogWrite(BIB_PIN, 0);
+}
